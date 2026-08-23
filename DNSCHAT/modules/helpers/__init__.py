7e5aba12144f735d177ac6b470d64dfe0469d63c
kalahume6-lgtm <kalahume6@gmail.com>
2026-08-23 08:16:35 +0530

    Update __init__.py

--- DNSCHAT/modules/helpers/__init__.py
+++ DNSCHAT/modules/helpers/__init__.py
@@ -3,10 +3,11 @@ from typing import Callable
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message
 
-from config import OWNER_ID  # <- OWNER yaha se le lo direct
+from config import OWNER_ID
+
 
 def is_admins(func: Callable) -> Callable:
-    async def non_admin(c, m: Message):  # <- DNSCHAT type hint hata diya
+    async def non_admin(c, m: Message):
         if m.from_user.id == OWNER_ID:
             return await func(c, m)
 
@@ -15,9 +16,9 @@ def is_admins(func: Callable) -> Callable:
             if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
                 return await func(c, m)
         except:
-            pass  # agar user group me nahi hai to ignore
+            pass
 
     return non_admin
 
-from .inline import *
-from .read import *
+     from .inline import *
+     from .read import *

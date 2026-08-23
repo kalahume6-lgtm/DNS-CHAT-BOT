2dcb8f17749aa6a30aac2a6d4c1820010697df0c
kalahume6-lgtm <kalahume6@gmail.com>
2026-08-23 12:20:31 +0530

    Update chats.py

--- DNSCHAT/database/chats.py
+++ DNSCHAT/database/chats.py
@@ -2,32 +2,37 @@ from DNSCHAT import db
 
 chatsdb = db.chatsdb
 
-
 async def get_served_chats() -> list:
-    chats = chatsdb.find({"chat_id": {"$lt": 0}})
-    if not chats:
-        return []
+    """
+    Saare served group chats ki list return karega
+    Sirf chat_id < 0 wale yani group/supergroup
+    """
     chats_list = []
-    for chat in await chats.to_list(length=1000000000):
+    async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
         chats_list.append(chat)
     return chats_list
 
-
 async def is_served_chat(chat_id: int) -> bool:
+    """
+    Check karega ki chat DB me hai ya nahi
+    """
     chat = await chatsdb.find_one({"chat_id": chat_id})
-    if not chat:
-        return False
-    return True
-
+    return chat is not None
 
 async def add_served_chat(chat_id: int):
+    """
+    Naya group add hone par DB me add karega
+    Duplicate nahi banega
+    """
     is_served = await is_served_chat(chat_id)
     if is_served:
         return
     return await chatsdb.insert_one({"chat_id": chat_id})
 
-
 async def remove_served_chat(chat_id: int):
+    """
+    Bot kick/leave hone par DB se hata dega
+    """
     is_served = await is_served_chat(chat_id)
     if not is_served:
         return
